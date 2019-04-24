cf99fd9cf144123c577b7f6bdc6853ed33115de1
denis96z <zinoviev.denis@yandex.ru>
2019-04-24 20:21:21 +0300

    oauth CLIENT_ID, CLIENT_SECRET added

--- config/config.go
+++ config/config.go
@@ -2,11 +2,15 @@ package config
 
 import (
 	"github.com/studtool/common/config"
+	"github.com/studtool/common/consts"
 )
 
 var (
 	ServerPort = config.NewStringDefault("STUDTOOL_VK_AUTH_SERVICE_PORT", "80")
 
+	OAuth2ClientId     = config.NewStringDefault("STUDTOOL_VK_OAUTH2_CLIENT_ID", consts.EmptyString)
+	OAuth2ClientSecret = config.NewStringDefault("STUDTOOL_VK_OAUTH2_CLIENT_SECRET", consts.EmptyString)
+
 	ShouldAllowCORS   = config.NewFlagDefault("STUDTOOL_VK_AUTH_SERVICE_SHOULD_ALLOW_CORS", true)
-	ShouldLogRequests = config.NewFlagDefault("STUDTOOL_VK_AUTH_SERVICE_SHOULD_LOG_REQUEST", true)
+	ShouldLogRequests = config.NewFlagDefault("STUDTOOL_VK_AUTH_SERVICE_SHOULD_LOG_REQUESTS", true)
 )

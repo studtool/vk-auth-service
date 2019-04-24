cab9256c04d9d5837b434bf44d983985c58b60f7
denis96z <zinoviev.denis@yandex.ru>
2019-04-24 23:20:14 +0300

    logs for env vars

--- config/config.go
+++ config/config.go
@@ -3,9 +3,19 @@ package config
 import (
 	"github.com/studtool/common/config"
 	"github.com/studtool/common/consts"
+
+	"github.com/studtool/vk-auth-service/beans"
 )
 
 var (
+	_ = func() *config.FlagVar {
+		f := config.NewFlagDefault("STUDTOOL_VK_AUTH_SERVICE_SHOULD_LOG_ENV_VARS", false)
+		if f.Value() {
+			config.SetLogger(beans.Logger)
+		}
+		return f
+	}()
+
 	ServerPort = config.NewStringDefault("STUDTOOL_VK_AUTH_SERVICE_PORT", "80")
 
 	OAuth2ClientId     = config.NewStringDefault("STUDTOOL_VK_OAUTH2_CLIENT_ID", consts.EmptyString)
